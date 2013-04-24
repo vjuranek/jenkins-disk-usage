d9838610fc69e62328e7d0c554e3407f14ec3303
Vojtech Juranek <vjuranek@redhat.com>
2013-04-25 00:18:47 +0200

    Few other sql queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,1 +1,3 @@
 select job as job_name,job_size as job_size from jobs order by job_size desc;
+select job as job_name, sum(artif_size) as asize from builds group by job_name order by asize desc;
+select job as job_name, sum(artif_size)as asize from builds where job='JOB_NAME' group by job_name;
