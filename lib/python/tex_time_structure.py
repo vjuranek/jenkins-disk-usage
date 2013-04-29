26fc2f72af510959280ff0316d939e77f113b195
Vojtech Juranek <vjuranek@redhat.com>
2013-04-29 23:05:04 +0200

    Switch from longtable to table

--- lib/python/tex_time_structure.py
+++ lib/python/tex_time_structure.py
@@ -8,22 +8,25 @@ def generate(file_path, days):
     sizes = db.size_of_builds_older_than(days)
     
     ft = open(file_path,"w")
+
+    ft.write("\\begin{table}[h!]\n")
+    ft.write("\\centering\n")
+    ft.write("\\begin{tabular}{|c|c|}\n")
+    ft.write("\t\\hline\n")
     
-    ft.write("\\begin{longtable}{|c|c|}\n")
+    ft.write("\t\\textbf{Builds older than X [days]} & \\textbf{Size [GB]}  \\\\ \n")
+
     ft.write("\t\\hline\n")
-    ft.write("\t\\textbf{Builds older than X [days]} & \\textbf{Size [GB]}  \\\\ \\hline\n")
     ft.write("\t\\hline\n")
-    ft.write("\t\\endhead\n")
-    ft.write("\t\\hline\n")
-    ft.write("\t\\endfoot\n")
-    ft.write("\t\\hline\n")
-    ft.write("\t\\caption{%s}\n"%CAPTION)
-    ft.write("\t\\endlastfoot\n")
-    
+     
     for item in sizes:
         print "%i\t%3.1f"%(item[0],item[1])
         ft.write("\t\t %i \t & \t  %3.1f"%(item[0], item[1]) + " \\\\\n")
         
-    ft.write("\\end{longtable}\n")
+    ft.write("\t\\hline\n")
+    ft.write("\\end{tabular}\n")
+    ft.write("\t\\caption{%s}\n"%CAPTION)
+    ft.write("\\end{table}\n")
+
 
     ft.close()
--- lib/python/tex_top_jobs.py
+++ lib/python/tex_top_jobs.py
@@ -10,22 +10,24 @@ def generate(file_path, limit):
     
     ft = open(file_path,"w")
     
-    ft.write("\\begin{longtable}{|c|c|}\n")
+    ft.write("\\begin{table}[h!]\n")
+    ft.write("\\centering\n")
+    ft.write("\\begin{tabular}{|c|c|}\n")
     ft.write("\t\\hline\n")
-    ft.write("\t\\textbf{Job name} & \\textbf{Size [GB]}  \\\\ \\hline\n")
+
+    ft.write("\t\\textbf{Job name} & \\textbf{Size [GB]}  \\\\ \n")
+
     ft.write("\t\\hline\n")
-    ft.write("\t\\endhead\n")
     ft.write("\t\\hline\n")
-    ft.write("\t\\endfoot\n")
-    ft.write("\t\\hline\n")
-    ft.write("\t\\caption{%s}\n"%CAPTION)
-    ft.write("\t\\endlastfoot\n")
     
     for item in jobs:
         print "%s\t%i"%(item[0],item[1])
         ft.write("\t\t %s \t & \t  %3.1f"%(u.tex_fix_str(item[0]), item[1]) + " \\\\\n")
-        
-    ft.write("\\end{longtable}\n")
+
+    ft.write("\t\\hline\n")
+    ft.write("\\end{tabular}\n")
+    ft.write("\t\\caption{%s}\n"%CAPTION)
+    ft.write("\\end{table}\n")
 
     ft.close()
 
