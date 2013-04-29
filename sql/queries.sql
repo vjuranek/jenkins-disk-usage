e721acfd6004385204a66ae65e26d6d9f164ef8e
Vojtech Juranek <vjuranek@redhat.com>
2013-04-29 23:06:45 +0200

    Update slq queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -13,4 +13,6 @@ select sum(build_size)/(1024^3) from builds where (time > (current_date - interv
 select sum(log_size)/(1024^3) from builds where (time > (current_date - interval '30 days'));
 select sum(artif_size)/(1024^3) from builds where (time > (current_date - interval '30 days'));
 
+select sum(build_size)/(1024^3), sum(artif_size)/(1024^3), sum(log_size)/(1024^3) from builds;
+
 select sum(job_size) from jobs where job in ('JOB1','JOB2');
