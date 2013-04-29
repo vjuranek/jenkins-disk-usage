17a34687803b081c3f3de7d99264f0bbab63834c
Vojtech Juranek <vjuranek@redhat.com>
2013-04-29 23:08:16 +0200

    Add generation of overall table

--- bin/tex_generate.py
+++ bin/tex_generate.py
@@ -3,6 +3,8 @@
 
 import tex_top_jobs as top_jobs
 import tex_time_structure as time_struct
+import tex_overview as overview
 
+overview.generate("../tex/generated/overview.tex")
 top_jobs.generate("../tex/generated/top_jobs.tex", 30)
 time_struct.generate("../tex/generated/time_struct.tex", [30,90,180,360,720])
