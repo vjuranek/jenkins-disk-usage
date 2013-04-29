3bf042f12b22a1e36fcb6f202669892892228714
Vojtech Juranek <vjuranek@redhat.com>
2013-04-29 18:43:44 +0200

    Add tex report with timescale of builds

--- bin/report.sh
+++ bin/report.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
+export PYTHONPATH="../lib/python:/home/vjuranek/tmp/tmp/jenkinsapi"
+
 ./tex_generate.py
 cd ../tex
 make
--- bin/tex_generate.py
+++ bin/tex_generate.py
@@ -2,5 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import tex_top_jobs as top_jobs
+import tex_time_structure as time_struct
 
 top_jobs.generate("../tex/generated/top_jobs.tex", 30)
+time_struct.generate("../tex/generated/time_struct.tex", [30,90,180,360,720])
